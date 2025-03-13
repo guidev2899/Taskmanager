58f6fb306d8f2271c6b5e7e9bc050005254bf6d1
guiav2899 <guilherme.dev2899@gmail.com>
2025-03-13 15:21:55 -0300

    Create function to list tasks

--- src/main/java/com/example/Taskmanager/TaskManager/Controller/TaskManagerController.java
+++ src/main/java/com/example/Taskmanager/TaskManager/Controller/TaskManagerController.java
@@ -7,10 +7,9 @@ import com.example.Taskmanager.TaskManager.TaskManagerDTO.TaskManagerDTO;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -31,6 +30,11 @@ public class TaskManagerController {
         );
         taskManagerService.createTask(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(taskManagerDTO);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<TaskManagerModel>> listTasks(){
+        return ResponseEntity.status(HttpStatus.FOUND).body(taskManagerService.listTasks());
     }
 
 }
